b28401ee0ade1a205dca30ee59b38326aacad084
joaogomes18 <j.gomes@ua.pt>
2021-01-29 18:33:03 +0000

    Data and jupyter

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import os
 import torch
 import torch.optim as optim
 import torch.backends.cudnn as cudnn
-import argparse
+#import argparse
 import torch.utils.data as data
 from data import WiderFaceDetection, detection_collate, preproc, cfg_mnet, cfg_re50
 from layers.modules import MultiBoxLoss
@@ -13,27 +13,37 @@ import datetime
 import math
 from models.retinaface import RetinaFace
 
-parser = argparse.ArgumentParser(description='Retinaface Training')
-parser.add_argument('--training_dataset', default='./data/widerface/train/label.txt', help='Training dataset directory')
-parser.add_argument('--network', default='mobile0.25', help='Backbone network mobile0.25 or resnet50')
-parser.add_argument('--num_workers', default=4, type=int, help='Number of workers used in dataloading')
-parser.add_argument('--lr', '--learning-rate', default=1e-3, type=float, help='initial learning rate')
-parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
-parser.add_argument('--resume_net', default=None, help='resume net for retraining')
-parser.add_argument('--resume_epoch', default=0, type=int, help='resume iter for retraining')
-parser.add_argument('--weight_decay', default=5e-4, type=float, help='Weight decay for SGD')
-parser.add_argument('--gamma', default=0.1, type=float, help='Gamma update for SGD')
-parser.add_argument('--save_folder', default='./weights/', help='Location to save checkpoint models')
+#parser = argparse.ArgumentParser(description='Retinaface Training')
+#parser.add_argument('--training_dataset', default='./data/widerface/train/label.txt', help='Training dataset directory')
+#parser.add_argument('--network', default='mobile0.25', help='Backbone network mobile0.25 or resnet50')
+#parser.add_argument('--num_workers', default=4, type=int, help='Number of workers used in dataloading')
+#parser.add_argument('--lr', '--learning-rate', default=1e-3, type=float, help='initial learning rate')
+#parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
+#parser.add_argument('--resume_net', default=None, help='resume net for retraining')
+#parser.add_argument('--resume_epoch', default=0, type=int, help='resume iter for retraining')
+#parser.add_argument('--weight_decay', default=5e-4, type=float, help='Weight decay for SGD')
+#parser.add_argument('--gamma', default=0.1, type=float, help='Gamma update for SGD')
+#parser.add_argument('--save_folder', default='./weights/', help='Location to save checkpoint models')
 
-args = parser.parse_args()
+#args = parser.parse_args()
 
-if not os.path.exists(args.save_folder):
-    os.mkdir(args.save_folder)
+#if not os.path.exists(args.save_folder):
+#    os.mkdir(args.save_folder)
+#cfg = None
+#if args.network == "mobile0.25":
+#    cfg = cfg_mnet
+#elif args.network == "resnet50":
+#    cfg = cfg_re50
+    
+if not os.path.exists('./weights/'):
+    os.mkdir('./weights/')
 cfg = None
-if args.network == "mobile0.25":
-    cfg = cfg_mnet
-elif args.network == "resnet50":
-    cfg = cfg_re50
+#if args.network == "mobile0.25":
+#    cfg = cfg_mnet
+#elif args.network == "resnet50":
+#    cfg = cfg_re50
+
+cfg = cfg_re50
 
 rgb_mean = (104, 117, 123) # bgr order
 num_classes = 2
@@ -43,21 +53,31 @@ batch_size = cfg['batch_size']
 max_epoch = cfg['epoch']
 gpu_train = cfg['gpu_train']
 
-num_workers = args.num_workers
-momentum = args.momentum
-weight_decay = args.weight_decay
-initial_lr = args.lr
-gamma = args.gamma
-training_dataset = args.training_dataset
-save_folder = args.save_folder
+#num_workers = args.num_workers
+#momentum = args.momentum
+#weight_decay = args.weight_decay
+#initial_lr = args.lr
+#gamma = args.gamma
+#training_dataset = args.training_dataset
+#save_folder = args.save_folder
+
+num_workers = 4
+momentum = 0.9
+weight_decay = 5e-4
+initial_lr = 1e-3
+gamma = 0.1
+training_dataset = './data/widerface/train/label.txt'
+save_folder = './weights/'
 
 net = RetinaFace(cfg=cfg)
 print("Printing net...")
 print(net)
 
-if args.resume_net is not None:
+#if args.resume_net is not None:
+if None is not None:
     print('Loading resume network...')
-    state_dict = torch.load(args.resume_net)
+    #state_dict = torch.load(args.resume_net)
+    state_dict = torch.load(None)
     # create new OrderedDict that does not contain `module.`
     from collections import OrderedDict
     new_state_dict = OrderedDict()
@@ -88,7 +108,8 @@ with torch.no_grad():
 
 def train():
     net.train()
-    epoch = 0 + args.resume_epoch
+    #epoch = 0 + args.resume_epoch
+    epoch = 0 + 0
     print('Loading Dataset...')
 
     dataset = WiderFaceDetection( training_dataset,preproc(img_dim, rgb_mean))
@@ -99,8 +120,10 @@ def train():
     stepvalues = (cfg['decay1'] * epoch_size, cfg['decay2'] * epoch_size)
     step_index = 0
 
-    if args.resume_epoch > 0:
-        start_iter = args.resume_epoch * epoch_size
+    #if args.resume_epoch > 0:
+    #    start_iter = args.resume_epoch * epoch_size
+    if 0 > 0:
+        start_iter = 0 * epoch_size
     else:
         start_iter = 0
 
